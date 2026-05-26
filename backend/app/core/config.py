5b4dfb7b711fff8edd8e996b0d7bd17e60703b4c
Yaswanth Sai Bhupalam <yaswanthsai.bhupalam@com>
2026-05-26 20:54:09 +0530

    Setup Production fastapi architecture

---
+++ backend/app/core/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str
+    API_V1_STR: str
+    DEBUG: bool
+
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+
+settings = get_settings()
---
+++ backend/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.core.config import settings
+from app.api.v1.api import api_router
+
+app=FastAPI(
+    title=settings.PROJECT_NAME
+)
+
+app.include_router(
+    api_router,
+    prefix=settings.API_V1_STR
+)
+
+@app.get('/')
+async def root():
+    return {"message": "Production RAG Chatbot API Running"}
